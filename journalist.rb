deca6f43c173d60a5018cfbfd8d7f0992f347e0a
mathieuchevet2 <mathieuchevet2@hotmail.fr>
2018-07-08 15:35:30 +0200

    mise en ligne

---
+++ journalist.rb
@@ -0,0 +1,129 @@
+journalistes = [ "@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+#Ajout d'une méthode dans l'instance de classe String
+class String
+  def numeric?
+    Float(self) != nil rescue false
+  end
+end
+
+#Affichage du nombre de journalistes
+def nombre_de_journaliste(journalistes)
+  puts "Il y a #{journalistes.size} journalistes dans le tableau."
+end
+
+def parcourt_journaliste(journalistes)
+  #initialise le compteur de handles contenant au moins un chiffre
+  @compteurElmtChiffre = 0
+  #initialise le compteur de handles commençant par une majuscule
+  @premiere_majuscule = 0
+  #initialise le compteur de handles contenant au moins une majuscule
+  @majuscule = 0
+  #initialise le compteur de handles contenant "aude"
+  @aude = 0
+  #initialise le compteur de handles contenant au moins un underscore
+  @underscore = 0
+
+    #Boucle qui appelle les fonctions qui prennent journaliste en paramètres
+    journalistes.each do |journaliste|
+    recherche_nombre(journaliste)
+    recherche_aude(journaliste)
+    recherche_premiere_majuscule(journaliste)
+    recherche_majuscules(journaliste)
+    recherche_underscore(journaliste)
+  end
+
+  #affichage des résultat
+  puts "Il y a #{@compteurElmtChiffre} journalistes avec un nombre dans le handle."
+  puts "Il y a #{@aude} journalistes nommés aude."
+  puts "Il y a #{@premiere_majuscule} handles qui commencent par une majuscule."
+  puts "Il y a #{@majuscule} handles qui contiennent une majuscule."
+  puts "Il y a #{@underscore} handles qui contiennent un underscore."
+
+end
+
+#fonction qui cherche les handles contenant au moins un chiffre
+def recherche_nombre(journaliste)
+  #initialise le compteur à 0
+  compteurChiffre = 0
+  #Transforme les caractères en ascii
+  journaliste.each_byte do |c|
+    #vérifie si chaque caractère est un nombre avec la fonction numeric?
+    if c.chr.numeric?
+      #ajoute 1 au compteur
+      compteurChiffre += 1
+    end
+  end
+#si un élément du tableau contient plus de 1 chiffres le compteur augmente du nombre de chiffres
+#que contient cet élément, ex: @Xavier75 -> 7, 5 -> compteur + 2
+#donc si le compteur est plus grand que 0
+  if compteurChiffre > 0
+    #le compteurElmtChiffre augmente de 1
+    @compteurElmtChiffre += 1
+  end
+
+end
+
+#fonction qui recherche le nombre de handles contenant "aude"
+def recherche_aude(journaliste)
+  #si l'élément du tableau en minuscule contient "aude"
+  if journaliste.downcase =~ /aude/
+    #alors aude + 1
+    @aude += 1
+  end
+
+end
+
+#fonction qui vérifie si la premiere lettre d'un handle est en majuscule
+def recherche_premiere_majuscule(journaliste)
+#si le caractère après le @ est en majuscule
+  if journaliste =~ /@[A-Z]/
+    #alors le compteur premiere_majuscule + 1
+    @premiere_majuscule += 1
+  end
+
+end
+#fonction qui vérifie si une majuscule est présente dans les handles
+def recherche_majuscules(journaliste)
+  #boucle qui transforme les caractère en ascii
+  journaliste.each_byte do |c|
+    #si le caractère en cours est en majuscule
+    if c.chr =~ /[A-Z]/
+      #alors compteur majuscule + 1
+      @majuscule += 1
+    end
+  end
+
+end
+#fonction qui vérifie si un underscore est présent dans les handles
+def recherche_underscore(journaliste)
+  #boucle qui transforme les caractère en ascii
+  journaliste.each_byte do |c|
+    #si le caractère en cours est un underscore
+    if c.chr =~ /_/
+      #alors compteur underscore + 1
+      @underscore += 1
+    end
+  end
+
+end
+#fonction qui trie les handles par ordre alphabétique
+def ordre_alphabetique(journalistes)
+  #Trie les handles par ordre alphabétique en ignorant la casse
+  journalistes.sort_by! { |journaliste| journaliste.downcase }
+  #affichage du résultats
+  puts "Voici la liste par ordre  alphabetque :"
+  puts journalistes
+
+end
+
+#fonction principale appelant toutes les fonctions
+def perform(journalistes)
+
+  ordre_alphabetique(journalistes)
+  parcourt_journaliste(journalistes)
+  nombre_de_journaliste(journalistes)
+
+end
+#appel de la fonction principale
+perform(journalistes)
